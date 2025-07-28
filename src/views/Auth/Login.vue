2b8a4343c2c1f4675f3994723adb7b9ae1bf426f
chinxzy <chinxzypoet@gmail.com>
2025-07-28 14:52:53 +0100

    commit

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="h-screen flex justify-center items-center relative flex">
-    <div
-      class="flex-none w-1/3 bg-login-image bg-cover bg-no-repeat h-full md:block hidden"
-    >
+    <div class="flex-none w-1/3 bg-login-image bg-cover bg-no-repeat h-full md:block hidden">
       <div class="pl-16">
         <div class="mt-8 mb-16">
           <a target="_blank" href="https://www.theoditor.com/"
@@ -22,9 +20,7 @@
         </div>
       </div>
 
-      <div
-        class="footer absolute bottom-10 text-primary-50 flex justify-center w-1/3 text-sm"
-      >
+      <div class="footer absolute bottom-10 text-primary-50 flex justify-center w-1/3 text-sm">
         <div>© 2024 Lintint Technology Limited</div>
         <div class="flex">
           <div class="ml-12 mr-2">
@@ -35,33 +31,23 @@
       </div>
     </div>
 
-    <div class="bg-white overflow-hidden flex-1">
+    <div class="bg-danger overflow-hidden flex-1">
       <div class="md:px-20 px-6 py-14 md:w-33rem mx-auto" id="modal-container">
         <div id="modal-container"></div>
 
         <div class="mb-8">
-          <img
-            src="../../assets/icons/Layer_x0020.svg"
-            alt=""
-            class="mx-auto"
-          />
+          <img src="../../assets/icons/Layer_x0020.svg" alt="" class="mx-auto" />
         </div>
-        <div class="text-2xl font-bold mb-4 text-center">
-          Login to your account
-        </div>
+        <div class="text-2xl font-bold mb-4 text-center">Login to your account</div>
         <div class="text-md mb-8 text-center text-lightgray">
           Welcome! We are committed to your business growth
         </div>
 
         <div class="mb-7">
-          <label
-            for="email"
-            class="block text-sm font-medium leading-6 text-gray-900"
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
             >Phone number / Email</label
           >
-          <div
-            class="flex items-center border border-gray-300 p-1 rounded-md h-14"
-          >
+          <div class="flex items-center border border-gray-300 p-1 rounded-md h-14">
             <input
               type="email"
               class="w-full px-2 py-1 outline-none"
@@ -78,14 +64,10 @@
         </div>
 
         <div class="mb-7">
-          <label
-            for="email"
-            class="block text-sm font-medium leading-6 text-gray-900"
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
             >Password</label
           >
-          <div
-            class="flex items-center border border-gray-300 p-1 rounded-md h-14"
-          >
+          <div class="flex items-center border border-gray-300 p-1 rounded-md h-14">
             <input
               :type="!showPass ? 'password' : 'text'"
               class="w-full px-2 py-1 outline-none"
@@ -134,69 +116,65 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { ref, computed, onBeforeMount } from "vue";
-import { useRouter } from "vue-router";
-import ButtonComp from "@/components/ui/button.vue";
-import InputError from "@/components/ui/inputError.vue";
-import apiError from "@/components/ui/apiError.vue";
-import { mailFormat, phoneNumberMaskRule } from "@/core/helpers/constants";
-import { openNotificationWithIcon } from "@/core/helpers/utility";
-import { useAuthStore, type loginCredential } from "@/stores/auth";
+import { ref, computed, onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
+import ButtonComp from '@/components/ui/button.vue'
+import InputError from '@/components/ui/inputError.vue'
+import apiError from '@/components/ui/apiError.vue'
+import { mailFormat, phoneNumberMaskRule } from '@/core/helpers/constants'
+import { openNotificationWithIcon } from '@/core/helpers/utility'
+import { useAuthStore, type loginCredential } from '@/stores/auth'
 
-const store = useAuthStore();
-const email = ref<string>("");
-const password = ref<string>("");
-const toastMessage = ref<string>("");
-const validEmail = ref<boolean>(true);
-const router = useRouter();
-const hasErrorEmail = ref<boolean>(false);
-const hasErrorPassword = ref<boolean>(false);
-const loginLoading = ref<boolean>(false);
-const showPass = ref<boolean>(false);
-const errorMessage = ref<string>("");
+const store = useAuthStore()
+const email = ref<string>('')
+const password = ref<string>('')
+const toastMessage = ref<string>('')
+const validEmail = ref<boolean>(true)
+const router = useRouter()
+const hasErrorEmail = ref<boolean>(false)
+const hasErrorPassword = ref<boolean>(false)
+const loginLoading = ref<boolean>(false)
+const showPass = ref<boolean>(false)
+const errorMessage = ref<string>('')
 const emptyField = computed(() => {
-  return email.value.trim() == "";
-});
+  return email.value.trim() == ''
+})
 const passWordEmpty = computed(() => {
-  return password.value.trim() == "";
-});
+  return password.value.trim() == ''
+})
 
 const contentText = ref<Array<string>>([
-  "Record sales.",
-  "Monitor your sales growth",
-  "Identify fast selling products",
-  "Monitor product activities",
-]);
+  'Record sales.',
+  'Monitor your sales growth',
+  'Identify fast selling products',
+  'Monitor product activities',
+])
 // onBeforeMount(() => {
 //   window.location.href = "https://theoditor.com/";
 // });
 
 const toggleShow = () => {
-  showPass.value = !showPass.value;
-};
+  showPass.value = !showPass.value
+}
 
 function ValidateFields() {
   if (
     (!emptyField.value && email.value.match(mailFormat)) ||
     email.value.match(phoneNumberMaskRule)
   ) {
-    validEmail.value = true;
+    validEmail.value = true
   } else {
-    validEmail.value = false;
+    validEmail.value = false
   }
 
-  if (
-    emptyField.value == false &&
-    validEmail.value == true &&
-    passWordEmpty.value == false
-  ) {
-    login();
+  if (emptyField.value == false && validEmail.value == true && passWordEmpty.value == false) {
+    login()
   } else {
     if (emptyField.value || !validEmail.value) {
-      hasErrorEmail.value = true;
+      hasErrorEmail.value = true
     }
     if (passWordEmpty.value) {
-      hasErrorPassword.value = true;
+      hasErrorPassword.value = true
     }
   }
 }
@@ -205,36 +183,31 @@ const login = async () => {
   const values = {
     username: email.value,
     password: password.value,
-  };
+  }
 
-  loginLoading.value = true;
+  loginLoading.value = true
   // Send login request
   await store
     .login(values)
     .then(() => {
-      openNotificationWithIcon(
-        "success",
-        null,
-        "You have successfully signed in!",
-        3
-      );
-      router.push("/pos");
+      openNotificationWithIcon('success', null, 'You have successfully signed in!', 3)
+      router.push('/pos')
     })
-    .finally(() => (loginLoading.value = false));
-};
+    .finally(() => (loginLoading.value = false))
+}
 
 async function showErrorMessage(error: string) {
-  errorMessage.value = error;
+  errorMessage.value = error
 }
 function closeToast() {
-  errorMessage.value = "";
+  errorMessage.value = ''
 }
 
 function goToForgotPassword() {
-  router.push("/forgotpassword");
+  router.push('/forgotpassword')
 }
 
 function showToast() {
-  toastMessage.value = "This is a timed toast message!";
+  toastMessage.value = 'This is a timed toast message!'
 }
 </script>
